b1a43bd635a544b3e91fb7df8533ff94580603be
Patel <kapatel@tree.com>
2021-08-09 19:51:41 +0530

    deploy jenkins

---
+++ main.tf
@@ -0,0 +1,171 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+        random = {
+      source = "hashicorp/random"
+    }
+  }
+    backend "remote" {
+    organization = "kaushik Patel"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
+}
+
+# EC2 Instance ##
+resource "aws_instance" "web" {
+  ami                    = "ami-09e67e426f25ce0d7"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.terraform_private_sg.id}"]
+  subnet_id              = aws_subnet.terraform-subnet_1.id
+  key_name               = "terraform-demo"
+  #user_data              = filebase64("jennkins-install.sh")
+  #  user_data                   = "${file("app_install.sh")}"
+  count                       = 1
+  associate_public_ip_address = true
+
+  provisioner "remote-exec" {
+    inline = [
+      "wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -",
+      "sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'",
+      "sudo apt update -qq",
+      "sudo apt install -y default-jre",
+      "sudo apt install -y jenkins",
+      "sudo systemctl start jenkins",
+      "sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080",
+      "sudo sh -c \"iptables-save > /etc/iptables.rules\"",
+      "echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections",
+      "echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections",
+      "sudo apt-get -y install iptables-persistent",
+      "sudo ufw allow 8080",
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file("D:/kube/terraform/ec2/devops-test.pem")
+  }
+
+  tags = {
+    Name = "Jenkins"
+  }
+}
+
+# VPC ##
+resource "aws_vpc" "terraform-vpc" {
+  cidr_block           = "172.16.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = "terraform-demo-vpc"
+  }
+}
+
+# Internet Gateway ##
+resource "aws_internet_gateway" "igw" {
+  #  vpc_id = "${aws_vpc.terraform-vpc.id}"
+  vpc_id = aws_vpc.terraform-vpc.id
+  tags = {
+    Name = "terraform-igw"
+  }
+}
+
+# subnet ##
+resource "aws_subnet" "terraform-subnet_1" {
+  vpc_id            = aws_vpc.terraform-vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-east-1a"
+  #  map_public_ip_on_launch = "true"
+  tags = {
+    Name = "terraform-subnet_1"
+  }
+}
+
+# Route Table ##
+resource "aws_route_table" "rtb_public" {
+  vpc_id = aws_vpc.terraform-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "terraform-rtb"
+  }
+}
+
+resource "aws_route_table_association" "rta_subnet_public" {
+  subnet_id      = aws_subnet.terraform-subnet_1.id
+  route_table_id = aws_route_table.rtb_public.id
+}
+
+## Security Group##
+resource "aws_security_group" "terraform_private_sg" {
+  description = "Allow limited inbound external traffic"
+  vpc_id      = aws_vpc.terraform-vpc.id
+  name        = "terraform_ec2_private_sg"
+
+  ingress {
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+  }
+
+  ingress {
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 8080
+    to_port     = 8080
+  }
+
+  ingress {
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 443
+    to_port     = 443
+  }
+
+  ingress {
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+  }
+
+  egress {
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+  }
+
+  tags = {
+    Name = "ec2-private-sg"
+  }
+}
+
+# EBS volume ##
+#resource "aws_ebs_volume" "example" {
+#  availability_zone = "us-east-1a"
+#  size              = 10
+#}
+
+#resource "aws_volume_attachment" "ebs_att" {
+#  device_name = "/dev/sdh"
+#  volume_id   = aws_ebs_volume.example.id
+#  instance_id = aws_instance.web[0].id
+#}
+
+# key-pair ##
+resource "aws_key_pair" "class" {
+  key_name   = "terraform-demo"
+  public_key = file("C:/Users/kapatel/devops-test.pub")
+}
